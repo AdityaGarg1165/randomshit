9fa1294cec497e7ab791c25d8045999702de7d62
AdityaGarg1165 <90836787+AdityaGarg1165@users.noreply.github.com>
2024-08-03 11:32:05 +0530

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -44,6 +44,14 @@ export default function Home() {
         </div>
       </div>
       </a>
+      <a target="_blank" href="https://questions.examside.com/past-years/jee/question/if-all-the-words-with-or-without-meaning-are-written-using-jee-main-mathematics-trigonometric-functions-and-equations-a1ym5tiywz4iydwu">
+      <div className="w-120 pl-14 pt-2 mt-9 h-12 rounded-3xl" style={{backgroundColor:"#151717"}}>
+        <div className="flex">
+            <img className="w-8 h-8 left-12 absolute" src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABwAAAAcCAMAAABF0y+mAAAAQlBMVEVHcEw8Y+s8Y+s8Y+s8Y+s8Y+s8Y+s8Y+s8Y+s8ZOs9ZOs0Xut4j/CNoPJxiu8qWOrN1fn///+crPNgfe7c4vu9x/f0X2FWAAAAC3RSTlMAN4PD4vxf7P8WFjaXv00AAACxSURBVHgBfJHFAcAgEMBwq7ew/6g1XPODwCkIQIQJpQQjCEoYFwHOMiWJyCAqiSgqYN9Fq+xpCtiz/CWxbl4cq7Xkc8w+3PbDsW/25quZB3muP5fvKFbzynmLKW1NKJHh3oIA7ksMSJTr9pKEJYBGadGblzSVu/k4pyjLsFsSdljQuBWYymII6fiWyxLHVw9eJ4P39c6nY0lWBmR72eoZvmRCKIFBkxF60uQknKjxZgcATS0Xn51y6MIAAAAASUVORK5CYII=" alt="" />
+            <h1 className="text-white">https://questions.examside.com/past-years/jee/question/</h1>
+        </div>
+      </div>
+      </a>
   
 
       </div>
