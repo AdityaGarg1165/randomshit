e7daded0f63bab0784819f307d32c47f238d20f7
AdityaGarg1165 <90836787+AdityaGarg1165@users.noreply.github.com>
2024-08-20 11:04:37 +0530

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -119,6 +119,14 @@ export default function Home() {
         </div>
       </div>
       </a>
+      <a id="a" className={"a"} target="_blank" href="https://questions.examside.com/past-years/jee/question/pa-particle-experiences-a-variable-force-overrightarrow-jee-main-physics-motion-yl8lo7olmqr1aneo">
+      <div className="w-120 pl-14 pt-2 mt-9 h-12 rounded-3xl">
+        <div className="flex">
+            <img className="w-8 h-8 left-12 absolute" src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABwAAAAcCAMAAABF0y+mAAAAQlBMVEVHcEw8Y+s8Y+s8Y+s8Y+s8Y+s8Y+s8Y+s8Y+s8ZOs9ZOs0Xut4j/CNoPJxiu8qWOrN1fn///+crPNgfe7c4vu9x/f0X2FWAAAAC3RSTlMAN4PD4vxf7P8WFjaXv00AAACxSURBVHgBfJHFAcAgEMBwq7ew/6g1XPODwCkIQIQJpQQjCEoYFwHOMiWJyCAqiSgqYN9Fq+xpCtiz/CWxbl4cq7Xkc8w+3PbDsW/25quZB3muP5fvKFbzynmLKW1NKJHh3oIA7ksMSJTr9pKEJYBGadGblzSVu/k4pyjLsFsSdljQuBWYymII6fiWyxLHVw9eJ4P39c6nY0lWBmR72eoZvmRCKIFBkxF60uQknKjxZgcATS0Xn51y6MIAAAAASUVORK5CYII=" alt="" />
+            <h1 className="text-white">https://questions.examside.com/past-years/jee/question</h1>
+        </div>
+      </div>
+      </a>
    
       
       <button  className="openAll mt-4 px-6 py-2 rounded-full bg-white text-black hover:bg-gray-200 transition-colors" onClick={()=>{
